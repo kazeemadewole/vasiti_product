b0a9eccd8819bbb347b2f795041f892711c454ae
Kazeem Adewole <adebayoadewole858@gmail.com>
2022-02-20 17:23:06 +0100

    Delete dist directory

--- dist/src/models/product-model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const db_1 = require("../config/db");
-const constants_1 = require("../utils/constants");
-class productModel extends db_1.ObjectionModel {
-}
-productModel.tableName = `${constants_1.SchemaName}.${constants_1.DefinedTableNames.PRODUCTS}`;
-exports.default = productModel;
--- dist/src/utils/constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SchemaName = exports.DefinedTableNames = void 0;
-exports.DefinedTableNames = {
-    PRODUCTS: 'products',
-};
-exports.SchemaName = 'product_service';
